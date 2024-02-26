c7f74d99502b0491a65bf81cd7ebe5e390841211
Sujata2059 <hreetuadhikari3@gmail.com>
2024-02-26 06:37:38 +0545

    Final

--- Assignment1/Greeter/Program.cs
+++ Assignment1/Greeter/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        // Assuming you have a variable named fullName
-        string fullName = "Sujata Adhikari";
+Console.WriteLine("Enter your Name");
+string fullName = Console.ReadLine().ToUpper();
 
-        // Create another variable cFullName and initialize it with fullName in uppercase
-        string cFullName = fullName.ToUpper();
+Console.WriteLine("Enter your Date of Birth as 2010/12/12");
+DateTime DOB = DateTime.Parse(Console.ReadLine());
 
-        // Print the uppercase name in the desired format
-        Console.WriteLine($"Hello, {cFullName} Ji!");
-    }
-}
+DateTime today = DateTime.Today;
+
+TimeSpan age = today - DOB;
+int totalDays = Convert.ToInt32(age.TotalDays);
+int totalHours = Convert.ToInt32(age.TotalHours);
+int totalMinutes = Convert.ToInt32(age.TotalMinutes);
+
+int years = totalDays / 365;
+int months = (totalDays % 365) / 30;
+int weeks = (totalDays % 365 % 30) / 7;
+int days = totalDays % 365 % 30 % 7;
+
+Console.WriteLine($"Hello, {fullName} Ji");
+
+Console.WriteLine($"Your DOB is {DOB.ToString("dddd, dd MMMM yyyy")}.");
+
+Console.WriteLine($"You are {years} years {months} months {weeks} weeks and {days} days old. \n");
+
+// EXTRA
+
+Console.WriteLine($"You are {totalDays} days old");
+Console.WriteLine($"You are {totalHours} hours old");
+Console.WriteLine($"You are {totalMinutes} minutes old");
